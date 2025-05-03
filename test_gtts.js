3ba423760bc4cc1347b673cfb1b4197a4234d7ae
ThantZin-Oo <thantzinoo17197@gmail.com>
2025-05-03 11:15:49 +0630

    Text_To_Speech_Ver0.1(removeTestFile)

--- test_gtts.js
+++
@@ -1,7 +0,0 @@
-const gTTS = require('gtts');
-const gtts = new gTTS('Hello world', 'en');
-
-gtts.save('test.mp3', function (err) {
-  if (err) throw err;
-  console.log('✅ test.mp3 saved successfully!');
-});
